767ecb07392b66e8b096665a1279c7f0fe4d6076
MARIA <m.g.f@live.com>
2022-09-09 20:34:51 +0200

    responsive

--- src.js
+++ src.js
@@ -9,13 +9,17 @@ let errorPhone = document.getElementsByClassName("errorPhone");
 
 
 submitBtn.addEventListener("click", (event) => {
+// PrecentDefault para la acción, en el paso específico, pero continúan los siguientes pasos
    if(inputMessage.value.length > 50){
         event.preventDefault();
+// con el block muestra el error
         errorMessage[0].style.display = "block";
    }else{
+// si no cu ple con la primera condición, no muestra el error
         errorMessage[0].style.display= "none";
    }
    if(inputPhone.value.slice(0,1) !== "+"){
+//evalua que en la posición 0,1 esté el signo +
     event.preventDefault();
     errorPhone[0].style.display = "block";
    }else{
